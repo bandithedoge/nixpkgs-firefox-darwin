0f36314b48af19054bdc2ac2f20c21245c990592
harukafractus <106440141+harukafractus@users.noreply.github.com>
2024-03-10 12:56:01 +0800

    Add librewolf support

--- overlay.nix
+++ overlay.nix
@@ -29,10 +29,38 @@ let
       };
     };
 
+  librewolfPackage = edition:
+    super.stdenv.mkDerivation rec {
+      inherit (sources."${edition}") version;
+      pname = "Librewolf";
+
+      buildInputs = [ super.pkgs.undmg ];
+      sourceRoot = ".";
+      phases = [ "unpackPhase" "installPhase" ];
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out/Applications
+        cp -r LibreWolf.app "$out/Applications/"
+
+        runHook postInstall
+      '';
+
+      src = super.fetchurl {
+        name = "Librewolf-${version}.dmg";
+        inherit (sources."${edition}") url sha256;
+      };
+
+      meta = {
+        description = "Mozilla Firefox, free web browser (binary package)";
+        homepage = "http://www.mozilla.com/en-US/firefox/";
+      };
+    };
 in {
   firefox-bin = firefoxPackage "firefox";
   firefox-beta-bin = firefoxPackage "firefox-beta";
   firefox-devedition-bin = firefoxPackage "firefox-devedition";
   firefox-esr-bin = firefoxPackage "firefox-esr";
   firefox-nightly-bin = firefoxPackage "firefox-nightly";
+  librewolf = if super.pkgs.system == "x86_64-darwin" then librewolfPackage "librewolf-x86_64" else librewolfPackage "librewolf-arm64";
 }
